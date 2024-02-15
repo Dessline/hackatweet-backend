f1b45997b3d913048eea3571097f1f6a38d5f99b
Dessline <desailles.dessline@outlook.fr>
2024-02-15 14:53:50 +0100

    first route and other modifications

--- models/tweets.js
+++ models/tweets.js
@@ -4,10 +4,10 @@ const tweetSchema = mongoose.Schema({
   content: String,
   date: Date,
   likes: Number,
-  users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'users' }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
   hashtags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'hashtags' }]
 });
 
-const User = mongoose.model('tweets', tweetSchema);
+const Tweet = mongoose.model('tweets', tweetSchema);
 
 module.exports = Tweet;
--- routes/tweets.js
+++ routes/tweets.js
@@ -1,4 +1,27 @@
 const Tweet = require('../models/tweets');
 const { checkBody } = require('../modules/checkBody');
+var express = require('express');
+var router = express.Router();
+
+router.post('/', (req, res) => {
+    if (!checkBody(req.body, ['content', 'user'])) {
+      res.json({ result: false, error: 'Missing or empty fields' });
+      return;
+    }
+
+        const newTweet = new Tweet({
+            content: req.body.content,
+            date: req.body.date,
+            likes: req.body.likes,
+            user: req.body.user,
+            hashtags: [req.body.hashtags]
+        });
+  
+        newTweet.save().then(newDoc => {
+          res.json({ result: true, content: newDoc.content });
+        });
+     
+    });
+  
 
 module.exports = router;
